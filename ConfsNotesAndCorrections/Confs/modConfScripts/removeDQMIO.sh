faa6828c1281d3a8017be7863f67badc988d00ab
Braden <braden.allmond@gmail.com>
2022-03-03 17:55:10 +0100

    forgot to delete old dirs

--- ConfsNotesAndCorrections/Confs/modConfScripts/removeDQMIO.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILE=$1
-
-# use sed to remove lines referring to timing and multithreading 
-# in place, match /pattern/ and delete(d) the next five lines (,+5)
-
-sed -i -e '/# enable TrigReport, TimeReport and MultiThreading/,+5d' $FILE;
-sed -i -e '/# load the DQMStore and DQMRootOutputModule/,+8d' $FILE;
