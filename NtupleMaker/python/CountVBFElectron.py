b19e6cd97eeb1d06e5507701bf872307c1e47beb
Braden Allmond <braden.allmond@gmail.com>
2022-08-14 09:38:52 -0500

    added two python skimmers i wrote to look at VBF plus electron data

---
+++ NtupleMaker/python/CountVBFElectron.py
@@ -0,0 +1,391 @@
+# Braden Allmond, July 1st 2022, KSU
+# (incomplete) tutorial: https://indico.cern.ch/event/704163/contributions/2936719/attachments/1693833/2726445/Tutorial-PyROOT.pdf
+from array import array
+import ROOT
+from ROOT import Math, TLorentzVector
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+# usage: python3 CountVBFElectron.py *.root
+# prints all event numbers and events passing a particular HLT
+# intended for use in easily finding rate of L1/HLT on EZB dataset
+
+ROOT.gROOT.SetBatch(True) # sets visual display off (i.e. no graphs/TCanvas)
+
+files = sys.argv # all input arguments in an array
+files.pop(0) # remove first arg which is always the name of the executed script
+
+
+def tallyPassL1s(nEvents, passDummyL1, passDummyL1Loose, passDummyL1ORDiJetSeeds, passDummyL1LooseORDiJetSeeds, passDummyL1OREGORL1s, passDummyL1LooseOREGORL1s, passDiJetSeeds, passEGORL1, passTripleOR, passTripleLooseOR, passOverlapOR):
+  print("Input Files")
+  for f in files:
+    print(f)
+    inFileName = f
+    inFile = ROOT.TFile.Open(inFileName,"READ")
+    tree = inFile.Get("demo/vbf")
+     
+
+    nEvents += tree.Draw("nEvents", "nEvents>0", "goff")
+    passDummyL1 += tree.Draw("nEvents", "passhltL1VBFElectron>0", "goff")
+    passDummyL1Loose += tree.Draw("nEvents", "passhltL1VBFElectronLoose>0", "goff")
+  
+    passDummyL1ORDiJetSeeds += tree.Draw("nEvents", "passhltL1VBFElectron>0 || passhltL1VBFDiJetOR>0 || passhltL1VBFDiJetIsoTau>0", "goff")
+    passDummyL1LooseORDiJetSeeds += tree.Draw("nEvents", "passhltL1VBFElectronLoose>0 || passhltL1VBFDiJetOR>0 || passhltL1VBFDiJetIsoTau>0", "goff")
+    passDummyL1OREGORL1s += tree.Draw("nEvents", "passhltL1VBFElectron>0 || passHLTDummyEGORL1>0", "goff")
+    passDummyL1LooseOREGORL1s += tree.Draw("nEvents", "passhltL1VBFElectronLoose>0 || passHLTDummyEGORL1>0", "goff")
+  
+    passDiJetSeeds += tree.Draw("nEvents", "passhltL1VBFDiJetOR>0 || passhltL1VBFDiJetIsoTau>0", "goff")
+    passEGORL1 += tree.Draw("nEvents", "passHLTDummyEGORL1>0", "goff")
+
+    passTripleOR += tree.Draw("nEvents", "passhltL1VBFElectron>0 || passhltL1VBFDiJetOR>0 || passhltL1VBFDiJetIsoTau>0 || passHLTDummyEGORL1>0", "goff")
+    passTripleLooseOR += tree.Draw("nEvents", "passhltL1VBFElectronLoose>0 || passhltL1VBFDiJetOR>0 || passhltL1VBFDiJetIsoTau>0 || passHLTDummyEGORL1>0", "goff")
+
+    passOverlapOR += tree.Draw("nEvents", "passhltL1VBFDiJetOR>0 || passhltL1VBFDiJetIsoTau>0 || passHLTDummyEGORL1>0", "goff")
+
+  print("Total EZB Counts")  
+  print("nEvents: {}".format(nEvents))
+  print("passDummyL1: {}".format(passDummyL1))
+  print("passDummyL1Loose: {}".format(passDummyL1Loose))
+  print("-----------------------")
+  print("pass DummyL1 OR other VBF Seeds: {}".format(passDummyL1ORDiJetSeeds))
+  print("pass DummyL1Loose OR other VBF Seeds: {}".format(passDummyL1LooseORDiJetSeeds))
+  print("pass other VBF Seeds: {}".format(passDiJetSeeds))
+  print("-----------------------")
+  print("pass DummyL1 exclusively not VBF: {}".format(passDummyL1ORDiJetSeeds - passDiJetSeeds))
+  print("pass DummyL1Loose exclusively not VBF: {}".format(passDummyL1LooseORDiJetSeeds - passDiJetSeeds))
+  print("-----------------------")
+  print("pass DummyL1 OR EGOR L1s: {}".format(passDummyL1OREGORL1s))
+  print("pass DummyL1Loose OR EGOR L1s: {}".format(passDummyL1LooseOREGORL1s))
+  print("pass EGOR L1 seeds: {}".format(passEGORL1))
+  print("-----------------------")
+  print("pass DummyL1 exclusively not other EG: {}".format(passDummyL1OREGORL1s - passEGORL1))
+  print("pass DummyL1Loose exclusively not other EG: {}".format(passDummyL1LooseOREGORL1s - passEGORL1))
+  print("-----------------------")
+  print("pass Triple OR: {}".format(passTripleOR))
+  print("pass TripleLoose OR: {}".format(passTripleLooseOR))
+  print("pass overlapOR: {}".format(passOverlapOR))
+  print("pass DummyL1 exclusively: {}".format(passTripleOR - passOverlapOR))
+  print("pass DummyLooseL1 exclusively: {}".format(passTripleLooseOR - passOverlapOR))
+  print("-----------------------")
+  print("Rate Constants")
+  print("nBunches = {}".format(nBunches))
+  print("revolution frequency = {} Hz".format(revfreq))
+  print("nBunches*rev.freq. = {} Hz".format(constFactor))
+  rateFactor = constFactor/nEvents
+  print("Rate Factor = nBunches*rev.freq./nEvents = {:.3f}".format(rateFactor))
+  print("Rate = Rate Factor * #Events Passing")
+
+  return rateFactor
+
+def plotVariable():
+
+  for f in files:
+    inFileName = f
+    inFile = ROOT.TFile.Open(inFileName, "READ")
+    tree = inFile.Get("demo/vbf")
+    elePt = ROOT.std.vector('float')()
+    tree.SetBranchAddress("hltL1VBFElectronLoose_ePt", elePt)
+
+def calcMjjAndJetIndices(inputJets):
+
+  for i in range(jetPt.size()):
+    TJet_ = ROOT.TLorentzVector(jetPt[i], jetEta[i], jetPhi[i], jetEnergy[i])
+    inputTJets.append(TJet_)
+
+  sizeTJets = len(inputTJets)
+  if (sizeTJets > 1):
+    mjj = 0 
+    mjjTemp = 0
+    leadingJet = -1
+    subleadingJet = -1
+    for j in range(sizeTJets):
+      for k in range(sizeTJets):
+        if (k > j):
+          tempJet1 = inputTJets[j]
+          tempJet2 = inputTJets[k]
+          mjjTemp = (tempJet1 + tempJet2).M()
+          #print(inputTJets[j].M())
+          if (mjjTemp > mjj):
+            mjj = mjjTemp
+            leadingJet = j
+            subleadingJet = k
+
+    leadingJetPt = inputTJets[leadingJet].Pt()
+    subleadingJetPt = inputTJets[subleadingJet].Pt()
+
+
+#passL1 = array('i', [0])
+#tree.SetBranchAddress("passhltL1VBFElectronLoose", passL1)
+
+def varyL1(electronScanRange_, jetScanRange_, mjjScanRange_, weight_, looseTrue):
+
+  electronScanRange = electronScanRange_
+  jetScanRange = jetScanRange_ 
+  mjjScanRange = mjjScanRange_ 
+  weight = weight_
+  gridTotal = np.zeros((3,11,11))
+  gridOverlap = np.zeros((3,11,11))
+
+  eLazyPt = []
+  mjjLazy = []
+
+  countPassLowestL1 = 0
+  countPassOtherVBFOR = 0
+  countPassEGORL1 = 0
+
+  print("Input Files")
+  for f in files:
+    print(f)
+    inFileName = f
+    inFile = ROOT.TFile.Open(inFileName,"READ")
+    tree = inFile.Get("demo/vbf")
+
+    passL1 = array('i', [0])
+    passIncVBF = array('i', [0])
+    passVBFIsoTau = array('i', [0])
+    passEGORL1Arr = array('i', [0])
+
+    jetPt = ROOT.std.vector('float')()
+    jetEta = ROOT.std.vector('float')()
+    jetPhi = ROOT.std.vector('float')()
+    jetEnergy = ROOT.std.vector('float')()
+    elePt = ROOT.std.vector('float')()
+
+    tree.SetBranchAddress("passhltL1VBFDiJetOR", passIncVBF)
+    tree.SetBranchAddress("passhltL1VBFDiJetIsoTau", passVBFIsoTau)
+    tree.SetBranchAddress("passHLTDummyEGORL1", passEGORL1Arr)
+
+    if (looseTrue):
+      tree.SetBranchAddress("passhltL1VBFElectronLoose", passL1)
+      tree.SetBranchAddress("hltL1VBFElectronLoose_jPt", jetPt)
+      tree.SetBranchAddress("hltL1VBFElectronLoose_jEta", jetEta)
+      tree.SetBranchAddress("hltL1VBFElectronLoose_jPhi", jetPhi)
+      tree.SetBranchAddress("hltL1VBFElectronLoose_jEnergy", jetEnergy)
+      tree.SetBranchAddress("hltL1VBFElectronLoose_ePt", elePt)
+      print("Using LooseIso Branches")
+    if (not looseTrue): 
+      tree.SetBranchAddress("passhltL1VBFElectron", passL1)
+      tree.SetBranchAddress("hltL1VBFElectron_jPt", jetPt)
+      tree.SetBranchAddress("hltL1VBFElectron_jEta", jetEta)
+      tree.SetBranchAddress("hltL1VBFElectron_jPhi", jetPhi)
+      tree.SetBranchAddress("hltL1VBFElectron_jEnergy", jetEnergy)
+      tree.SetBranchAddress("hltL1VBFElectron_ePt", elePt)
+      print("Not using LooseIso Branches")
+
+
+    for entry in range(tree.GetEntriesFast()):
+    #for entry in range(5000):
+      #print(i)
+      tree.GetEntry(entry)
+      passOtherVBFOR = 0
+      passEGORL1 = 0
+      if (passIncVBF[0] == 1 or passVBFIsoTau[0] == 1):
+        passOtherVBFOR = 1
+        countPassOtherVBFOR += 1
+
+      if (passEGORL1Arr[0] == 1): 
+        passEGORL1 = 1
+        countPassEGORL1 += 1
+
+      if passL1[0] == 1:
+        # print("Entry {}".format(entry))
+        # find highest mjj pair
+        inputTJets = []
+        for i in range(jetPt.size()):
+
+          TJet_ = ROOT.TLorentzVector(jetPt[i], jetEta[i], jetPhi[i], jetEnergy[i])
+          inputTJets.append(TJet_)
+
+        sizeTJets = len(inputTJets)
+        if (sizeTJets > 1):
+          mjj = 0 
+          mjjTemp = 0
+          leadingJet = -1
+          subleadingJet = -1
+          for j in range(sizeTJets):
+            for k in range(sizeTJets):
+              if (k > j):
+                tempJet1 = inputTJets[j]
+                tempJet2 = inputTJets[k]
+                mjjTemp = (tempJet1 + tempJet2).M()
+                #print(inputTJets[j].M())
+                if (mjjTemp > mjj):
+                  mjj = mjjTemp
+                  leadingJet = j
+                  subleadingJet = k
+
+        leadingJetPt = inputTJets[leadingJet].Pt()
+        subleadingJetPt = inputTJets[subleadingJet].Pt()
+
+        electronPt = elePt[0]
+
+        #eLazyPt.append(electronPt)
+        #mjjLazy.append(mjj)
+
+        #print(eLazyPt)
+        #print(mjjLazy)
+
+        #print("===========")
+        #print("leadingJetPt    : {:.1f}".format(leadingJetPt))
+        #print("subleadingJetPt : {:.1f}".format(subleadingJetPt))
+        #print("mjj             : {:.1f}".format(mjj))
+        #print("electronPt      : {:.1f}".format(electronPt))
+        #print("[{:.1f},{:.1f},{:.1f},{:.1f}]".format(leadingJetPt,subleadingJetPt,mjj,electronPt))
+
+        for eleIndex, eleEntry in enumerate(electronScanRange):
+          for jetIndex, jetEntry in enumerate(jetScanRange):
+            for mjjIndex, mjjEntry in enumerate(mjjScanRange):
+
+                #if not ((electronPt >= eleEntry) and (leadingJetPt >= jetEntry and subleadingJetPt >= jetEntry) and (mjj >= mjjEntry)):
+                  #print("[{:.1f},{:.1f},{:.1f},{:.1f}]".format(leadingJetPt,subleadingJetPt,mjj,electronPt))
+                  #print(sizeTJets)
+                #countPassLowestL1 += 1
+
+              if ((electronPt >= eleEntry) and (leadingJetPt >= jetEntry and subleadingJetPt >= jetEntry) and (mjj >= mjjEntry)):
+                gridTotal[eleIndex, jetIndex, mjjIndex] += weight
+                gridOverlap[eleIndex, jetIndex, mjjIndex] += weight
+
+                if (eleEntry == 10 and jetEntry == 30 and mjjEntry == 300):
+                  countPassLowestL1 += 1
+
+                if (passOtherVBFOR or passEGORL1):
+                  gridOverlap[eleIndex, jetIndex, mjjIndex] -= weight
+
+
+  print("Sanity check, nEvents passing lowest simulated L1 (known discrepancy): {}".format(countPassLowestL1))
+  print("Sanity check, nEvents passing OR of other VBF L1s: {}".format(countPassOtherVBFOR))
+  print("Sanity check, nEvents passing OR of other EG L1s:  {}".format(countPassEGORL1))
+
+  #neweLazyPt = []
+  #for i in range(len(eLazyPt)):
+    #if (mjjLazy[i]>300):
+      #neweLazyPt.append(eLazyPt[i])
+
+  #lazy = True
+  #if (lazy):
+    #fig, ax = plt.subplots()
+    #ax.plot(eLazyPt, mjjLazy)
+    #ax.plot(electronScanRange, eLazyPt)
+    #ax.hist(neweLazyPt, 20)
+    #plt.show()
+
+ 
+
+  return gridTotal, gridOverlap
+
+if __name__ == "__main__":
+
+  nEvents = 0
+  passDummyL1 = 0
+  passDummyL1Loose = 0
+  passDummyL1ORDiJetSeeds = 0
+  passDummyL1LooseORDiJetSeeds = 0
+  passDummyL1OREGORL1s = 0
+  passDummyL1LooseOREGORL1s = 0
+  passDiJetSeeds = 0
+  passEGORL1 = 0
+  passTripleOR = 0
+  passTripleLooseOR = 0
+  passOverlapOR = 0
+
+  nBunches = 2736
+  revfreq = 11245.6 # Hz
+  constFactor = nBunches*revfreq
+
+  testRate = True
+  rateFactor = 0
+  if (testRate):
+    rateFactor = tallyPassL1s(nEvents, passDummyL1, passDummyL1Loose, passDummyL1ORDiJetSeeds, passDummyL1LooseORDiJetSeeds, passDummyL1OREGORL1s, passDummyL1LooseOREGORL1s, passDiJetSeeds, passEGORL1, passTripleOR, passTripleLooseOR, passOverlapOR)
+    print(rateFactor)
+
+  electronScanRange = np.linspace(10.,14.,3)
+  jetScanRange = np.linspace(30.,50.,11)
+  mjjScanRange = np.linspace(300.,500.,11)
+
+  useLooseIso = False
+  gridOutTotal, gridOutOvRm = varyL1(electronScanRange, jetScanRange, mjjScanRange, rateFactor, useLooseIso)
+  #eLazyOut, mjjLazyOut = varyL1(electronScanRange, jetScanRange, mjjScanRange, rateFactor, useLooseIso)
+
+  plot = True
+  if (plot):
+    intJetPt = [int(i) for i in jetScanRange]
+    intMjj = [int(i) for i in mjjScanRange]
+  
+    gridmax = np.max(gridOutTotal)
+    gridmax = gridmax*1.2
+  
+    fig, axes = plt.subplots(2, 3)
+    fig.set_size_inches(10.5, 10.5)
+  
+    for i, ax in enumerate(axes.flat):
+
+ 
+      if (i == 0 or i == 1 or i == 2):
+        im = ax.imshow(gridOutTotal[i], vmin=0, vmax=gridmax, cmap='copper', interpolation='nearest', origin='lower')
+        ax.set_title("electronPt >= " + str(2*(i)+10), fontsize=8)
+        for (n,m),label in np.ndenumerate(gridOutTotal[i]):
+          label = "{:.1f}".format(label/1000.)
+          ax.text(m, n, label,ha='center',va='center', color='white', fontsize=6)
+
+      if (i == 3 or i == 4 or i == 5):
+        im = ax.imshow(gridOutOvRm[i-3], vmin=0, vmax=gridmax, cmap='copper', interpolation='nearest', origin='lower')
+        ax.set_xlabel('jetPt >=')
+        for (n,m),label in np.ndenumerate(gridOutOvRm[i-3]):
+          label = "{:.1f}".format(label/1000.)
+          ax.text(m, n, label,ha='center',va='center', color='white', fontsize=6)
+
+
+      if (i == 2):
+        ax.text(1.05, 0.55, "Total", transform=ax.transAxes, fontsize=12,
+          rotation=-90)
+
+      if (i == 5):
+        ax.text(1.05, 0.25, "Overlap Removed", transform=ax.transAxes, fontsize=12,
+          rotation=-90)
+
+      ax.set_ylabel('mjj >=')
+  
+      start, end = ax.get_xlim()
+  
+      ax.xaxis.set_ticks(np.arange(start+0.5, end, 1))
+      ax.xaxis.set_ticklabels(intJetPt)
+      ax.yaxis.set_ticks(np.arange(start+0.5, end, 1))
+      ax.yaxis.set_ticklabels(intMjj)
+
+      if (i == 1 or i == 2 or i == 4 or i == 5):
+        ax.get_yaxis().set_visible(False)
+       
+  
+  
+    plt.savefig('output.pdf')
+    plt.show()
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ NtupleMaker/python/L1VBFEleEff.py
@@ -0,0 +1,215 @@
+# Braden Allmond, August 10th 2022, KSU
+
+from array import array
+import ROOT
+#from ROOT import Math, TLorentzVector
+#import sys
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+ROOT.gROOT.SetBatch(True) # sets visual display off (i.e. no graphs/TCanvas)
+
+
+def highestMjjPair(inObjs):
+  '''
+  Takes in an array of TLorentzVector objects
+  Returns the array indices of the highest mjj pair of objects and the mjj
+    
+    Parameters:
+      inObjs - an array of TLorentzVectors
+
+    Returns:
+      leadingJetIndex - the array index of the higher pt jet in the mjj pair
+      subleadingJetIndex - the array index of the lower pt jet in the mjj pair
+      mjj - the dijet mass of the highest mjj pair of the inObjs
+  '''
+  nObjs = len(inObjs) 
+
+  mjj = 0 
+  mjjTemp = 0
+  leadingJetIndex = -1
+  subleadingJetIndex = -1
+
+  for j in range(nObjs):
+    for k in range(nObjs):
+      if (k > j):
+        tempJet1 = inObjs[j]
+        tempJet2 = inObjs[k]
+        mjjTemp = (tempJet1 + tempJet2).M()
+        if (mjjTemp > mjj):
+          mjj = mjjTemp
+          leadingJetIndex = j
+          subleadingJetIndex = k
+
+  return leadingJetIndex, subleadingJetIndex, mjj
+
+def fillWithTVecs(branchPt, branchEta, branchPhi, branchEnergy, arrayIDs=None):
+  '''
+  Takes in four kinematic branch names
+  Returns an array of TLorentzVector objects filled with the kinematic info
+
+    Parameters:
+      branchPt -
+      branchEta -
+      branchPhi -
+      branchEnergy -
+
+    Return:
+      outputTVecs - 
+  '''
+  if arrayIDs is None:
+    arrayIDs = range(len(branchPt))
+
+  outputTVecs = []
+  #for i in range(branchPt.size()):
+  for i in arrayIDs:
+    tempVec = ROOT.TLorentzVector(branchPt[i], branchEta[i], branchPhi[i], branchEnergy[i])
+    outputTVecs.append(tempVec)
+  return outputTVecs
+
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser(description='Open a .root file')
+  parser.add_argument('-i', '--inputRootFile', dest='inFilename', action='store',
+                    help='the input .root file\'s name')
+  args = parser.parse_args()
+
+  inFile = ROOT.TFile.Open(args.inFilename,"READ")
+  tree = inFile.Get("demo/vbf")
+
+  # hell to read but
+  # defining a variable/object handle 
+  #   objName = type('typename')(initialization*) *initialization changes by type
+  # and then linking that object handle to an available branch in the event tree
+  #   tree.SetBranchAddress("branchOfObjInTree", objHandle)
+
+  # L1
+  # try with primitives at some point to compare...
+  #eleL1PrimitivesPt
+  #eleL1PrimitivesEta
+  #eleL1PrimitivesPhi
+  #eleL1PrimitivesEnergy
+  #etc
+  passL1 = array('i', [0])
+  tree.SetBranchAddress("passhltL1VBFElectronLoose", passL1)
+  L1JetPt = ROOT.std.vector('float')()
+  tree.SetBranchAddress("hltL1VBFElectron_jPt", L1JetPt)
+  L1JetEta = ROOT.std.vector('float')()
+  tree.SetBranchAddress("hltL1VBFElectron_jEta", L1JetEta)
+  L1JetPhi = ROOT.std.vector('float')()
+  tree.SetBranchAddress("hltL1VBFElectron_jPhi", L1JetPhi)
+  L1JetEnergy = ROOT.std.vector('float')()
+  tree.SetBranchAddress("hltL1VBFElectron_jEnergy", L1JetEnergy)
+  L1ElePt = ROOT.std.vector('float')()
+  tree.SetBranchAddress("hltL1VBFElectron_ePt", L1ElePt)
+  L1EleEta = ROOT.std.vector('float')()
+  tree.SetBranchAddress("hltL1VBFElectron_eEta", L1EleEta)
+  L1ElePhi = ROOT.std.vector('float')()
+  tree.SetBranchAddress("hltL1VBFElectron_ePhi", L1ElePhi)
+  L1EleEnergy = ROOT.std.vector('float')()
+  tree.SetBranchAddress("hltL1VBFElectron_eEnergy", L1EleEnergy)
+  
+  # HLT
+
+  # Offline kinems
+  ##Taus
+  OffnTaus = array('i', [0])
+  tree.SetBranchAddress("nTau", OffnTaus)
+  OffTauPt = ROOT.std.vector('float')()
+  tree.SetBranchAddress("tauPt", OffTauPt)
+  OffTauEta = ROOT.std.vector('float')()
+  tree.SetBranchAddress("tauEta", OffTauEta)
+  OffTauPhi = ROOT.std.vector('float')()
+  tree.SetBranchAddress("tauPhi", OffTauPhi)
+  OffTauEnergy = ROOT.std.vector('float')()
+  tree.SetBranchAddress("tauEnergy", OffTauEnergy)
+  ##Jets
+  OffnJets = array('i', [0])
+  tree.SetBranchAddress("nJet", OffnJets)
+  OffJetPt = ROOT.std.vector('float')()
+  tree.SetBranchAddress("jetPt", OffJetPt)
+  OffJetEta = ROOT.std.vector('float')()
+  tree.SetBranchAddress("jetEta", OffJetEta)
+  OffJetPhi = ROOT.std.vector('float')()
+  tree.SetBranchAddress("jetPhi", OffJetPhi)
+  OffJetEnergy = ROOT.std.vector('float')()
+  tree.SetBranchAddress("jetEn", OffJetEnergy)
+  ##Electrons
+  OffnEles = array('i', [0])
+  tree.SetBranchAddress("nEle", OffnEles)
+  OffElePt = ROOT.std.vector('float')()
+  tree.SetBranchAddress("elePt", OffElePt)
+  OffEleEta = ROOT.std.vector('float')()
+  tree.SetBranchAddress("eleEta", OffEleEta)
+  OffElePhi = ROOT.std.vector('float')()
+  tree.SetBranchAddress("elePhi", OffElePhi)
+  OffEleEnergy = ROOT.std.vector('float')()
+  tree.SetBranchAddress("eleEn", OffEleEnergy)
+
+  # Offline Ids
+  OffJetPFLooseID = ROOT.std.vector('bool')()
+  tree.SetBranchAddress("jetPFLooseId", OffJetPFLooseID)
+  OffJetID = ROOT.std.vector('int')()
+  tree.SetBranchAddress("jetID", OffJetID)
+  OffJetPUID = ROOT.std.vector('float')()
+  tree.SetBranchAddress("jetPUID", OffJetPUID)
+  OffJetPUFullID = ROOT.std.vector('int')()
+  tree.SetBranchAddress("jetPUFullID", OffJetPUFullID)
+
+  OffEleID = ROOT.std.vector('int')()
+  tree.SetBranchAddress("eleIDMVANoIsowp90", OffEleID)
+
+  for entry in range(5000):
+    #print(entry)
+    tree.GetEntry(entry)
+
+    # check if event passes L1 and HLT and basic offline object count requirements
+    basicReqs = passL1[0] and (OffnJets[0] >= 2) and (OffnEles[0] >= 1)
+    if basicReqs:
+
+      # make and fill containers with TLorentzVectors
+      # we will match the offline to the L1s, so it is redudant to cut on both
+      # subsequently, we make cuts on the tighter (offline) set to begin with
+
+      L1Jets = fillWithTVecs(L1JetPt, L1JetEta, L1JetPhi, L1JetEnergy)
+      sizeL1Jets = len(L1Jets)
+      L1Eles = fillWithTVecs(L1ElePt, L1EleEta, L1ElePhi, L1EleEnergy)
+      sizeL1Eles = len(L1Eles)
+
+      preOffJets = fillWithTVecs(OffJetPt, OffJetEta, OffJetPhi, OffJetEnergy)#, PassJetID)
+      JetPtToPass = 45
+      PassJetPt =  [i for i in range(len(OffJetPt)) if OffJetPt[i] >= JetPtToPass]
+      PassJetID =  [i for i in range(len(OffJetID)) if OffJetID[i] >= 2]
+      PassJetEta = [i for i in range(len(OffJetEta)) if OffJetEta[i] <= 4.7]
+      # python list intersection https://stackoverflow.com/questions/3697432/how-to-find-list-intersection
+      # listed in the order which removes the most members first (assuming & short-circuits)
+      OffJetsPassFilter = list(set(PassJetPt) & set(PassJetID) & set(PassJetEta))
+      OffJets = [preOffJets[i] for i in OffJetsPassFilter]
+      sizeOffJets = len(OffJets)
+
+      preOffEles = fillWithTVecs(OffElePt, OffEleEta, OffElePhi, OffEleEnergy)#, PassEleID)
+      ElePtToPass = 12
+      PassElePt =  [i for i in range(len(OffElePt)) if OffElePt[i] >= ElePtToPass]
+      PassEleID =  [i for i in range(len(OffEleID)) if OffEleID[i] >= 1]      
+      PassEleEta = [i for i in range(len(OffEleEta)) if OffEleEta[i] <= 2.1]
+      OffElesPassFilter = list(set(PassEleID) & set(PassElePt) & set(PassEleEta))
+      OffEles = [preOffEles[i] for i in OffElesPassFilter]
+      sizeOffEles = len(OffEles)
+
+      # as written, it would just make more sense to assign things all at once, instead of before and then skimming
+
+      if (sizeL1Jets > 1): #only call function if more than 2 objects
+        L1LeadingJetIndex, L1SubleadingJetIndex, L1mjj = highestMjjPair(L1Jets)
+
+      allGood = False
+      if (allGood):
+        L1LeadingJetPt = L1Jets[leadingJetIndex].Pt()
+        L1SubleadingJetPt = L1Jets[subleadingJetIndex].Pt()
+
+        electronPt = L1ElePt[0]
+
+
+  
+
