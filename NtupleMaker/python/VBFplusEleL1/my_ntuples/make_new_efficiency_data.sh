3eea3aab94303f90e9a677d81f4af7461de42e97
Braden Allmond <braden.allmond@gmail.com>
2023-01-29 05:18:09 -0600

    modifications to reintegrate gain/eff calculations

--- NtupleMaker/python/VBFplusEleL1/my_ntuples/make_new_efficiency_data.sh
+++ NtupleMaker/python/VBFplusEleL1/my_ntuples/make_new_efficiency_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 INPUT=../../../../../samples/VBFE_wMuTauFilters.root
-OUTFILE=output_Jan_13th.txt
+OUTFILE=output_2ndpass_loose_Jan_27th.txt
 
-for i in {10..15};
+for i in {1..5};
 do
 echo "#####################################################" >> $OUTFILE;
-python3 L1VBFEleEff.py -i $INPUT -L $i -s tight >> $OUTFILE;
-echo "#####################################################" >> $OUTFILE;
-python3 L1VBFEleEff.py -i $INPUT -L $i -s loose >> $OUTFILE;
+python3 L1VBFEleEff.py -i $INPUT -r "NOTRATE" -L $i -s loose >> $OUTFILE;
+#echo "#####################################################" >> $OUTFILE;
+#python3 L1VBFEleEff.py -i $INPUT -L $i -s loose >> $OUTFILE;
 done;
 
