fadfff63c1f9c11a0c5e7c3b4c8832665678d893
Braden <braden.allmond@gmail.com>
2023-03-30 16:25:05 +0200

    cleaning crab directory

--- NtupleMaker/crab/crabNoPy.py
+++ NtupleMaker/crab/crabNoPy.py
@@ -12,18 +12,24 @@ config = config()
 config.General.workArea = 'test'
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'hltUpdatedVBFEGsMC.py'
+config.JobType.psetName = 'hltNewMCJEC.py'
 
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
+config.Data.splitting = 'FileBased' #Automatic works better for the other MC
 config.Data.unitsPerJob = 1
 config.Data.outLFNDirBase = '/store/user/ballmond'
 
 config.Data.ignoreLocality = True
-config.Site.whitelist = ['T2_CH_CSCS','T1_US_FNAL', 'T2_FR_GRIF_LLR', 'T2_HU_Budapest', 'T3_US_FNALLPC', 'T2_CH_CERN', 'T2_BE_UCL', 'T2_DE_RWTH', 'T2_FR_IPHC', 'T2_IN_TIFR', 'T2_IT_Pisa', 'T2_UK_London_IC', 'T2_US_Wisconsin', 'T2_IT_Rome', 'T2_EE_Estonia', 'T2_UK_SGrid_RALPP']
+config.Site.whitelist = ['T2_CH_CSCS', 'T1_US_FNAL', 'T2_FR_GRIF_LLR', 'T2_HU_Budapest', 'T3_US_FNALLPC', 
+                         'T2_CH_CERN', 'T2_BE_UCL', 'T2_DE_RWTH', 'T2_FR_IPHC', 'T2_IN_TIFR', 'T2_IT_Pisa', 
+                         'T2_UK_London_IC', 'T2_US_Wisconsin', 'T2_IT_Rome', 'T2_EE_Estonia', 'T2_UK_SGrid_RALPP', 
+                         'T2_UK_SGrid_Bristol', 'T2_AT_Vienna']
 config.JobType.maxJobRuntimeMin = 2000
 config.JobType.maxMemoryMB = 4000
-config.JobType.inputFiles = ['L1Menu_Collisions2022_v1_3_0_edited_corrected.xml']
+config.JobType.inputFiles = ['L1Menu_Collisions2022_v1_4_0_splitVBF2jets.xml',
+'MC_corrections/PFCalibration.db',
+'MC_corrections/Run3Winter23Digi.db',
+'MC_corrections/Run3Winter23Digi_onlyAK4.db']
 
 
 config.Site.storageSite = 'T3_US_FNALLPC'
@@ -41,18 +47,14 @@ config.Site.storageSite = 'T3_US_FNALLPC'
 
 
     #VBF
-#the AOD with the premix is the MiniAOD's parent dataset, so this should be fine
-config.General.requestName = 'MC_03062023'
-config.Data.inputDataset = '/VBFHToTauTau_M125_TuneCP5_14TeV-powheg-pythia8/Run3Summer21MiniAOD-120X_mcRun3_2021_realistic_v5-v2/MINIAODSIM'
-config.Data.secondaryInputDataset = '/VBFHToTauTau_M125_TuneCP5_14TeV-powheg-pythia8/Run3Summer21DRPremix-120X_mcRun3_2021_realistic_v6-v2/GEN-SIM-DIGI-RAW'   
+config.General.requestName = 'NewMCSampleExtension_withJECv3'
+#New MC
+#config.Data.inputDataset = '/VBFHToTauTau_M125_TuneCP5_13p6TeV_powheg-pythia8/Run3Winter23MiniAOD-126X_mcRun3_2023_forPU65_v1-v2/MINIAODSIM'
+#config.Data.secondaryInputDataset = '/VBFHToTauTau_M125_TuneCP5_13p6TeV_powheg-pythia8/Run3Winter23Digi-126X_mcRun3_2023_forPU65_v1-v2/GEN-SIM-RAW'
+#New MC extension
+config.Data.inputDataset = '/VBFHToTauTau_M125_TuneCP5_13p6TeV_powheg-pythia8/Run3Winter23MiniAOD-126X_mcRun3_2023_forPU65_v1_ext1-v2/MINIAODSIM'
+config.Data.secondaryInputDataset = '/VBFHToTauTau_M125_TuneCP5_13p6TeV_powheg-pythia8/Run3Winter23Digi-126X_mcRun3_2023_forPU65_v1_ext1-v2/GEN-SIM-RAW'
 
-    #ggH
-#config.Data.inputDataset = '/GluGluHToTauTau_M-125_TuneCP5_14TeV-powheg-pythia8/Run3Summer21MiniAOD-120X_mcRun3_2021_realistic_v5-v2/MINIAODSIM'
-#config.Data.secondaryInputDataset = '/GluGluHToTauTau_M-125_TuneCP5_14TeV-powheg-pythia8/Run3Summer21DRPremix-120X_mcRun3_2021_realistic_v6-v2/GEN-SIM-DIGI-RAW'
-
-    # newest ggH
-#config.Data.inputDataset = '/GluGluHToTauTau_M-125_TuneCP5_14TeV-powheg-pythia8/Run3Winter21DRMiniAOD-FlatPU30to80FEVT_112X_mcRun3_2021_realistic_v16-v1/MINIAODSIM'
-#config.Data.secondaryInputDataset = '/GluGluHToTauTau_M-125_TuneCP5_14TeV-powheg-pythia8/Run3Winter21DRMiniAOD-FlatPU30to80FEVT_112X_mcRun3_2021_realistic_v16-v1/GEN-SIM-DIGI-RAW'
-    # newest VBF
-#config.Data.inputDataset = '/VBFHToTauTau_M125_TuneCP5_14TeV-powheg-pythia8/Run3Winter21DRMiniAOD-FlatPU30to80FEVT_112X_mcRun3_2021_realistic_v16-v1/MINIAODSIM'
-#config.Data.secondaryInputDataset = '/VBFHToTauTau_M125_TuneCP5_14TeV-powheg-pythia8/Run3Winter21DRMiniAOD-FlatPU30to80FEVT_112X_mcRun3_2021_realistic_v16-v1/GEN-SIM-DIGI-RAW'
+#Old MC
+#config.Data.inputDataset = '/VBFHToTauTau_M125_TuneCP5_14TeV-powheg-pythia8/Run3Summer21MiniAOD-120X_mcRun3_2021_realistic_v5-v2/MINIAODSIM'
+#config.Data.secondaryInputDataset = '/VBFHToTauTau_M125_TuneCP5_14TeV-powheg-pythia8/Run3Summer21DRPremix-120X_mcRun3_2021_realistic_v6-v2/GEN-SIM-DIGI-RAW'   
